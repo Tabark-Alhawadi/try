cc112b50103922eac68247dba99421681c1f7a3b
TabarkAlhawadi <tabarkalhawadi1@gmail.com>
2022-12-31 21:00:55 +0300

    index

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<?php
+
+$country = "india";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>TEST</h1>
+
+<script>
+
+    var country="<?php $country ?>";
+
+    alert('Welcome to' + country);
+</script>
+</body>
+</html>
